43b9f79e871e920f58e747f324b68de86f347c51
Javier <javiervalenzuelarivas@gmail.com>
2026-04-13 21:50:41 -0400

    avance de proyecto, falta incorporar imagenes en formato liviano

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from "react"
+import "./styles.css"
+
+const splitIntoColumns = (images, count) => {
+    const cols = Array.from({ length: count }, () => [])
+
+    images.forEach((img, i) => {
+        cols[i % count].push(img)
+    })
+
+    return cols
+}
 
 const App = () => {
-    const [count, setCount] = useState(0)
+    const columns = splitIntoColumns([
+        "https://api.pcloud.com/getpubthumb?code=XZdaO65ZfPKyXYSh9uJ4Svy9RJXTEXjKWEB7&linkpassword=&size=1024x1024&crop=0&type=auto",
+        "https://api.pcloud.com/getpubthumb?code=XZcaO65ZROUSO83nCFhY4v92XvqNvSocIDV7&linkpassword=&size=1024x1024&crop=0&type=auto"
+    ], 4)
 
     return (
-        <>
-            <h1>It works!</h1>
-        </>
+        <div className="wall">
+            {columns.map((col, i) => {
+                const loop = [...col, ...col]
+
+                return (
+                    <div key={i} className={`col col-${i}`}>
+                        {loop.map((src, j) => (
+                            <img key={j} src={src} loading="lazy" />
+                        ))}
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
